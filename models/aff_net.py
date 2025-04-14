2f7e18aa471a5dfaf0f248b854d8aa28378b922e
sigma10010 <yibaosun@gmail.com>
2025-04-14 15:23:01 +0800

    support selective kernel nets

--- models/aff_net.py
+++ models/aff_net.py
@@ -292,12 +292,17 @@ class AFFDelta(nn.Module):
         return {'delta': x, 'gaze': gaze}
 
 if __name__ == '__main__':
-    m = AFFModel()
-    feature = {"left": torch.zeros(10,1, 36,60),
-                "right": torch.zeros(10,1, 36,60)
-                }
-    feature = {"faceImg": torch.zeros(10, 3, 224, 224), "leftEyeImg": torch.zeros(10, 3, 112, 112),
-               "rightEyeImg": torch.zeros(10, 3, 112, 112), "faceGridImg": torch.zeros(10, 25, 25),
-               "label": torch.zeros(10, 2), "frame": "test.jpg"}
-    a = m(feature["leftEyeImg"], feature["rightEyeImg"], feature["faceImg"], feature["faceGridImg"])
-    print(a.shape)
+    # m = AFFModel()
+    # feature = {"left": torch.zeros(10,1, 36,60),
+    #             "right": torch.zeros(10,1, 36,60)
+    #             }
+    # feature = {"faceImg": torch.zeros(10, 3, 224, 224), "leftEyeImg": torch.zeros(10, 3, 112, 112),
+    #            "rightEyeImg": torch.zeros(10, 3, 112, 112), "faceGridImg": torch.zeros(10, 25, 25),
+    #            "label": torch.zeros(10, 2), "frame": "test.jpg"}
+    # a = m(feature["leftEyeImg"], feature["rightEyeImg"], feature["faceImg"], feature["faceGridImg"])
+    # print(a.shape)
+
+    x = torch.rand(8, 3, 224, 224)
+    conv = FaceImageModel()
+    out = conv(x)
+    print('out shape : {}'.format(out.shape))
--- models/sk.py
+++ models/sk.py
@@ -5,10 +5,11 @@ from torch import nn
 
 
 class SKConv(nn.Module):
-    def __init__(self, features, WH=None, M=2, G=1, r=4, stride=1 ,L=32):
-        """ Constructor
+    def __init__(self, in_channel,out_channel, WH=None, M=2, G=1, r=4, stride=1 ,L=32):
+        """ Constructor 
         Args:
-            features: input channel dimensionality.
+            in_channel: input channel dimensionality.
+            out_channel: output channel dimensionality.
             WH: input spatial dimensionality, used for GAP kernel size.
             M: the number of branchs.
             G: num of convolution groups.
@@ -17,29 +18,29 @@ class SKConv(nn.Module):
             L: the minimum dim of the vector z in paper, default 32.
         """
         super(SKConv, self).__init__()
-#         d = max(int(features/r), L)
-        d = int(features/r)
+        # d = max(int(out_channel/r), L)
+        d = int(out_channel/r)
         self.M = M
-        self.features = features
+        # self.features = features
         self.convs = nn.ModuleList([])
         for i in range(M):
             self.convs.append(nn.Sequential(
-                nn.Conv2d(features, features, kernel_size=3+i*2, stride=stride, padding=1+i, groups=G),
-                nn.BatchNorm2d(features),
+                nn.Conv2d(in_channel, out_channel, kernel_size=3+i*2, stride=stride, padding=1+i, groups=G),
+                nn.BatchNorm2d(out_channel),
                 nn.ReLU(inplace=False)
             ))
         # self.gap = nn.AvgPool2d(int(WH/stride))
-        self.fc = nn.Linear(features, d)
+        self.fc = nn.Linear(out_channel, d)
         self.fcs = nn.ModuleList([])
         for i in range(M):
             self.fcs.append(
-                nn.Linear(d, features)
+                nn.Linear(d, out_channel)
             )
         self.softmax = nn.Softmax(dim=1)
         
     def forward(self, x):
         for i, conv in enumerate(self.convs):
-            fea = conv(x).unsqueeze_(dim=1)
+            fea = conv(x).unsqueeze(dim=1)
             if i == 0:
                 feas = fea
             else:
@@ -61,41 +62,23 @@ class SKConv(nn.Module):
 
 
 class SKUnit(nn.Module):
-    def __init__(self, in_features, out_features, WH, M, G, r, mid_features=None, stride=1, L=32):
-        """ Constructor
-        Args:
-            in_features: input channel dimensionality.
-            out_features: output channel dimensionality.
-            WH: input spatial dimensionality, used for GAP kernel size.
-            M: the number of branchs.
-            G: num of convolution groups.
-            r: the radio for compute d, the length of z.
-            mid_features: the channle dim of the middle conv with stride not 1, default out_features/2.
-            stride: stride.
-            L: the minimum dim of the vector z in paper.
-        """
+    def __init__(self, in_features, out_features, WH=None, M=2, G=1, r=4, stride=1, L=32, is_shortcut=True):
         super(SKUnit, self).__init__()
-        if mid_features is None:
-            mid_features = int(out_features/2)
         self.feas = nn.Sequential(
-            nn.Conv2d(in_features, mid_features, 1, stride=1),
-            nn.BatchNorm2d(mid_features),
-            SKConv(mid_features, WH, M, G, r, stride=stride, L=L),
-            nn.BatchNorm2d(mid_features),
-            nn.Conv2d(mid_features, out_features, 1, stride=1),
-            nn.BatchNorm2d(out_features)
+            SKConv(in_features, out_features, WH=WH, M=M, G=G, r=r, stride=stride, L=L)
         )
-        if in_features == out_features: # when dim not change, in could be added diectly to out， Identity Shortcut
+        if in_features == out_features: # Identity Shortcut
             self.shortcut = nn.Sequential()
-        else: # when dim not change, in should also change dim to be added to out， Projection Shortcut
+        else: # Projection Shortcut
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_features, out_features, 1, stride=stride),
                 nn.BatchNorm2d(out_features)
             )
+        self.is_shortcut = is_shortcut
     
     def forward(self, x):
         fea = self.feas(x)
-        return fea + self.shortcut(x)
+        return fea + self.shortcut(x) if self.is_shortcut else fea
 
 
 class SKNet(nn.Module):
@@ -147,11 +130,7 @@ class SKNet(nn.Module):
 
 
 if __name__=='__main__':
-    x = torch.rand(8, 64, 32, 32)
-    conv = SKConv(64, 32, 3, 8, 2)
+    x = torch.rand(8, 32, 112, 112)
+    conv = SKConv(32,30)
     out = conv(x)
-    criterion = nn.L1Loss()
-    loss = criterion(out, x)
-    loss.backward()
     print('out shape : {}'.format(out.shape))
-    print('loss value : {}'.format(loss))
